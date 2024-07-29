54655e98190cad77d16ce1e58eef34feac4d6ed8
Iowa28 <yazzzik@list.ru>
2024-07-29 09:08:26 +0300

    remove template classes

--- Source/MultiplayerShooter/MultiplayerShooterGameMode.cpp
+++ Source/MultiplayerShooter/MultiplayerShooterGameMode.cpp
@@ -1,14 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "MultiplayerShooterGameMode.h"
-#include "MultiplayerShooterCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AMultiplayerShooterGameMode::AMultiplayerShooterGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Character/BP_Character"));
-	if (PlayerPawnBPClass.Class != NULL)
+	if (PlayerPawnBPClass.Class)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
