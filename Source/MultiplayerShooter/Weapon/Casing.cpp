21bb33d3ec949708862f42b46805601b585ed4bf
Iowa28 <yazzzik@list.ru>
2024-08-22 11:06:52 +0300

    shell physics and sound

--- Source/MultiplayerShooter/Weapon/Casing.cpp
+++ Source/MultiplayerShooter/Weapon/Casing.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACasing::ACasing()
 {
@@ -9,9 +11,42 @@ ACasing::ACasing()
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+
+	CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	CasingMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	ShellEjectionImpulse = 10.f;
+	DestroyDelay = 3.f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+
+	if (UWorld* World = GetWorld())
+	{
+		FTimerHandle TimerHandle;
+		FTimerDelegate Delegate;
+		Delegate.BindUFunction(this, "DestroyShell");
+		World->GetTimerManager().SetTimer(TimerHandle, Delegate, DestroyDelay, false);
+	}
+}
+
+void ACasing::DestroyShell()
+{
+	Destroy();
 }
--- Source/MultiplayerShooter/Weapon/Casing.h
+++ Source/MultiplayerShooter/Weapon/Casing.h
@@ -17,7 +17,22 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Shell")
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Shell")
+	float DestroyDelay;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Shell")
+	class USoundCue* ShellSound;
+
+	UFUNCTION()
+	void DestroyShell();
 };
