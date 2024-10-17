1de8305d597b7572047bee4055211ec97ae878ff
Iowa28 <yazzzik@list.ru>
2024-10-17 14:28:07 +0300

    pickup class

---
+++ Source/MultiplayerShooter/Pickup/Pickup.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "MultiplayerShooter/Types/WeaponTypes.h"
+#include "Sound/SoundCue.h"
+
+APickup::APickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	OverlapSphere->SetupAttachment(RootComponent);
+	OverlapSphere->SetSphereRadius(75.f);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	PickupMesh->SetupAttachment(OverlapSphere);
+	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	PickupMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_PURPLE);
+}
+
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+	}
+}
+
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APickup::Destroyed()
+{
+	Super::Destroyed();
+
+	if (PickupSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(
+			this,
+			PickupSound,
+			GetActorLocation()
+		);
+	}
+}
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                              UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1,15.f,FColor::Red, TEXT("Pickup overlaps"));
+}
